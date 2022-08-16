d649b992821688237c470b9d953e7c582c935226
Bobi Basari <bobi.basari@bvarta.com>
2022-08-16 12:06:53 +0700

    change next config and package json for build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['raw.githubusercontent.com'],
+    domains: ['https://pokeapi.co/api/v2/pokemon'],
+    loader: 'akamai',
+    path: '',
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId },
+  ) {
+    return {
+      '/': { page: '/' },
+      '/bag': { page: '/bag' },
+    };
   },
 };
 
